c3861231910a47a7ed8c8f732e7f0e64557a8c4e
sudhakarsamak <sudhakarsamak@users.noreply.github.com>
2018-10-11 20:31:42 -0400

    BandIt | Band Listing Page

--- BandIt/Pages/BandListing.cshtml.cs
+++ BandIt/Pages/BandListing.cshtml.cs
@@ -19,7 +19,30 @@ namespace BandIt.Pages
         public ICollection<Band> Bands { get; set; }
         public void OnGet()
         {
+            SearchCompleted = false;
+            
             Bands = _context.Band.OrderBy(x => x.BandName).Include(x => x.BandManager).ToList();
+        }
+
+
+        [BindProperty]
+        public string Search { get; set; }
+        public bool SearchCompleted { get; set; }
+        public ICollection<Band> SearchResults { get; set; }
+        
+        public void OnPost() {
+            // PERFORM SEARCH
+            if (string.IsNullOrWhiteSpace(Search)) {
+                SearchResults =  _context.Band.OrderBy(x => x.BandName).Include(x => x.BandManager).ToList();
+                SearchCompleted = true;
+            }
+            else{
+                SearchResults = _context.Band
+                                    .Include(x => x.BandManager)
+                                    .Where(x => x.BandName.ToLower().Contains(Search.ToLower()))
+                                    .ToList();
+                SearchCompleted = true;
+            }
         }
     }
 }
